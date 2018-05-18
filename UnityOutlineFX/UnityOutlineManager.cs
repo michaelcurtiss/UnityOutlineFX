ea9766ab2ee7f207532c4919989e59dc60757dbf
Michael Curtiss <michaelvcurtiss@gmail.com>
2018-05-18 17:10:07 -0400

    Initial Commit

---
+++ UnityOutlineFX/UnityOutlineManager.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Xenu.Game {
+
+	public class UnityOutlineManager : MonoBehaviour {
+		[System.Serializable]
+		public class OutlineData
+		{
+			public Renderer renderer;
+		}
+
+		public UnityOutlineFX outlinePostEffect;
+		public OutlineData[] outliners;
+
+		private void Start()
+		{
+			foreach (var obj in outliners)
+			{
+				outlinePostEffect.AddRenderers(new List<Renderer>() { obj.renderer });
+			}
+		}
+	}
+
+}
